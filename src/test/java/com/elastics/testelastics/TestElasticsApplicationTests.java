4f7dbe6e4cbb634b90d37880029bac7075b84259
hamza5word <hamza_hm@hotmail.be>
2021-12-04 23:49:01 +0100

    ADD Comment to test

--- src/test/java/com/elastics/testelastics/TestElasticsApplicationTests.java
+++ src/test/java/com/elastics/testelastics/TestElasticsApplicationTests.java
@@ -14,6 +14,7 @@ class TestElasticsApplicationTests {
 
     @Test
     public void testInitData() {
+        // TEST DATA
         Assertions.assertDoesNotThrow(() -> service.initUsers());
         Assertions.assertDoesNotThrow(() -> service.initItems());
     }
