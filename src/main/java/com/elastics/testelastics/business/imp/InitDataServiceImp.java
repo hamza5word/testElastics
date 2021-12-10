60927091cb749a657d38f8e8832f794d17e17c93
hamza5word <hamza_hm@hotmail.be>
2021-12-10 18:28:06 +0100

    Improve initItems methods

--- src/main/java/com/elastics/testelastics/business/imp/InitDataServiceImp.java
+++ src/main/java/com/elastics/testelastics/business/imp/InitDataServiceImp.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Service
@@ -29,12 +30,13 @@ public class InitDataServiceImp implements InitDataService {
 
     @Override
     public void initItems() {
-        userRepository.findAll().forEach(u -> Stream.of("s21", "dell latitude").forEach(item -> {
+        List<User> users = userRepository.findAll();
+        Stream.of("Galaxy s21", "Iphone 12", "Asus ROG Strix 17", "Samsung LED 50", "Apple Watch").forEach(item -> {
             Item i = new Item();
             i.setTitle(item);
             i.setQuantity(new SecureRandom().nextInt() * 100);
-            i.setUser(u);
+            i.setUser(users.get(new SecureRandom().nextInt() * users.size()));
             itemRepository.save(i);
-        }));
+        });
     }
 }
